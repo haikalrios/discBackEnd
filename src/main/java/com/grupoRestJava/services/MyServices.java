125d40a7c11e62abdbe598901d2a8c44b6dcd551
Haikal Rios <Haikal@cooperforte.coop>
2017-08-25 15:12:27 -0300

    primeiro commit

---
+++ src/main/java/com/grupoRestJava/services/MyServices.java
@@ -0,0 +1,15 @@
+package com.grupoRestJava.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/myServices")
+public class MyServices {
+
+	@GET()
+	@Path("/ola/{nome}")
+	public String ola(@PathParam("nome") String nome ){
+		return "Ola "+ nome;
+	}
+}
