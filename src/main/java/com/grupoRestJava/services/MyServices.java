a28f7072da21739e7c3e34eee4e8ae3a435f6b91
root <root@house>
2017-09-02 14:42:36 -0300

    Implementações post, put e delete

--- src/main/java/com/grupoRestJava/services/MyServices.java
+++ src/main/java/com/grupoRestJava/services/MyServices.java
@@ -1,6 +1,9 @@
 package com.grupoRestJava.services;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
@@ -8,9 +11,27 @@ import javax.ws.rs.PathParam;
 @Path("/myServices")
 public class MyServices {
 
-	@GET()
+	@GET
 	@Path("/ola/{nome}")
-	public String ola(@PathParam("nome") String nome ){
+	public String olaGet(@PathParam("nome") String nome ){
+		return "Ola "+ nome;
+	}
+	
+	@POST
+	@Path("/ola/{nome}")
+	public String olaPost(@PathParam("nome") String nome ){
+		return "Ola "+ nome;
+	}
+	
+	@PUT
+	@Path("/ola/{nome}")
+	public String olaPut(@PathParam("nome") String nome ){
+		return "Ola "+ nome;
+	}
+	
+	@DELETE
+	@Path("/ola/{nome}")
+	public String olaDelete(@PathParam("nome") String nome ){
 		return "Ola "+ nome;
 	}
 }
