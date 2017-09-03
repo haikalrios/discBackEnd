2f2c5a016bc58e1da1bd4051118eb4f7d1c7f385
Your Name <you@example.com>
2017-09-02 21:28:29 -0300

    metodos put delete

--- src/main/java/com/grupoRestJava/AS/LivrosAS.java
+++ src/main/java/com/grupoRestJava/AS/LivrosAS.java
@@ -14,31 +14,73 @@ public class LivrosAS {
 	@EJB
 	private LivroDao livroDao;
 
-	public Livro salvar(Livro statusProspect) throws Exception {
-		if (statusProspect == null) {
+	public Livro addLivro(Livro livro) throws Exception {
+		if (livro == null) {
 			throw new Exception("Preciso passa o livro");
 		}
-		if (statusProspect.getDescricao() == null) {
+		if (livro.getDescricao() == null) {
 			throw new Exception("Preciso passar a decsrição");
 		}
 
-		if (statusProspect.getIsbn() == null) {
+		if (livro.getIsbn() == null) {
 			throw new Exception("Preciso passr o ISBN");
 		}
 
-		if (statusProspect.getSubTitulo() == null) {
+		if (livro.getSubTitulo() == null) {
 			throw new Exception("Preciso passar o subtitulo");
 		}
-		if (statusProspect.getTitulo() == null) {
+		if (livro.getTitulo() == null) {
 			throw new Exception("Preciso passar o titulo");
 		}
 
-		livroDao.persist(statusProspect);
+		livroDao.persist(livro);
 
-		return statusProspect;
+		return livro;
 
 	}
 
+	public Livro updateLivro(Livro livro) throws Exception {
+		
+		if (livro == null) {
+			throw new Exception("Preciso passa o livro");
+		}
+		if (livro.getDescricao() == null) {
+			throw new Exception("Preciso passar a decsrição");
+		}
+		if (livro.getId()== null) {
+			throw new Exception("Preciso passar o id");
+		}
+
+		if (livro.getIsbn() == null) {
+			throw new Exception("Preciso passr o ISBN");
+		}
+
+		if (livro.getSubTitulo() == null) {
+			throw new Exception("Preciso passar o subtitulo");
+		}
+		if (livro.getTitulo() == null) {
+			throw new Exception("Preciso passar o titulo");
+		}
+
+		Livro livroAtualizado = livroDao.findById(livro.getId());
+		
+		if (livroAtualizado.getId()== null) {
+			throw new Exception("Livro não existe");
+		}
+		livroAtualizado.setDescricao(livro.getDescricao());
+		livroAtualizado.setId(livro.getId());
+		livroAtualizado.setIsbn(livro.getIsbn());
+		livroAtualizado.setSubTitulo(livro.getSubTitulo());
+		livroAtualizado.setTitulo(livro.getTitulo());
+		
+		
+		livroDao.persist(livroAtualizado);
+
+		return livro;
+
+	}
+	
+	
 	public List<Livro> listAll() throws Exception {
 		return livroDao.findAll();
 
@@ -51,6 +93,15 @@ public class LivrosAS {
 		Livro l = livroDao.findById(id);
 		return l;
 
+	}
+
+	public Livro delete(Integer id) throws Exception {
+		Livro l = livroDao.findById(id);
+		if (l == null) {
+			throw new Exception("Livro não existe");
+		}
+		livroDao.remove(l);
+		return l ;
 	}
 
 }
--- src/main/java/com/grupoRestJava/services/LivroFCD.java
+++ src/main/java/com/grupoRestJava/services/LivroFCD.java
@@ -2,11 +2,14 @@ package com.grupoRestJava.services;
 
 import java.util.List;
 
+import javax.decorator.Delegate;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -42,79 +45,23 @@ public class LivroFCD {
 	@POST
 	@Path("/")
 	public Livro addLivro(Livro livro) throws Exception{
-		return livroAS.salvar(livro);
-	}
-	
-	/*@GET
-	@Path("/pessoa/{id}")
-	public PessoaTO listarPessoa(@PathParam("id") Integer id) throws Exception{
-		PessoaTO s = new  PessoaTO();
-		s.setId(1L);
-		s.setNome("Haikal");
-		return s;
-	}
-	a
-	@POST
-	@Path("/addPessoa")
-	public void addStatus(PessoaTO p){
-		System.out.println(p);
-	}
-	
-	*/
-/*	
-	
-	
-	@GET
-	@Path("/{id}")
-	public PessoaTO olaGet(@PathParam("id") Long id ){
-		PessoaTO p = new PessoaTO();
-		p.setId(id);
-		p.setNome("Joao "+ String.valueOf(id) );
-		return p;
-	}
-	
-	@GET
-	@Path("/ola/{id}")
-	public String olaGetString(@PathParam("id") String id ){
-		return "Haikal";
-	}
-	
-	@POST
-	@Path("/add")
-	public PessoaTO olaPost(PessoaTO p){
-		p.setId(10L);
-		p.setNome("Joao 10 " );
-		return p;
+		return livroAS.addLivro(livro);
 	}
 	
 	@PUT
-	@Path("/{id}")
-	public PessoaTO olaPut(@PathParam("id") Long id, PessoaTO p){
-		if (p == null){
-			p = new PessoaTO();
-		}
-		p.setId(id);
-		p.setNome(p.getNome() + " Metodo com id");
-		return p ;
+	@Path("/")
+	public Livro updateLivro( Livro l) throws Exception{
+		
+		return livroAS.updateLivro(l) ;
 	}
 	
-	@PUT
-	public PessoaTO olaPut( PessoaTO p){
-		p.setNome(p.getNome() + " Metodo sem id");
-		return p ;
-	}
-
-
 	
 	@DELETE
 	@Path("/{id}")
-	public PessoaTO olaDelete(@PathParam("id") String nome ){
-		PessoaTO p = new PessoaTO();
-		p.setId(10L);
-		p.setNome("Joao 10 " );
-		return p;
+	public Livro deleteLivro(@PathParam("id") Integer id) throws Exception{
+		return livroAS.delete(id) ;
 	}
-	*/
+
 	
 
 }
