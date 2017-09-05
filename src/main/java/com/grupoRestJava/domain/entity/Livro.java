b328e833a7d89d2797b6ec27824ddcc39de12e2b
rafael <rafael.pivetta@gmail.com>
2017-09-04 23:21:14 -0300

    Inclusao dos campos autor e status

--- src/main/java/com/grupoRestJava/domain/entity/Livro.java
+++ src/main/java/com/grupoRestJava/domain/entity/Livro.java
@@ -11,64 +11,85 @@ import javax.persistence.Table;
 @Table(name = "TB_LIVRO")
 public class Livro extends BaseEntidade<Integer> {
 
-   private static final long serialVersionUID = 492472753851383923L;
+	private static final long serialVersionUID = 492472753851383923L;
 
-   @Id
-   @GeneratedValue(strategy=GenerationType.IDENTITY)
-   @Column(name = "PK_Livro")
-   private Integer           id;
-   
-   @Column(name = "Isbn", nullable = false)
-   private String           isbn;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "PK_Livro")
+	private Integer           id;
 
-   @Column(name = "Descricao", length = 200, nullable = false)
-   private String            descricao;
+	@Column(name = "Isbn", nullable = false)
+	private String           isbn;
 
-   @Column(name = "titulo",length = 60, nullable = false)
-   private String           titulo;
-   
-   @Column(name = "subtitulo",length = 60, nullable = false)
-   private String           subTitulo;
+	@Column(name = "Descricao", length = 200, nullable = false)
+	private String            descricao;
 
-public Integer getId() {
-	return id;
-}
+	@Column(name = "titulo",length = 60, nullable = false)
+	private String           titulo;
 
-public void setId(Integer id) {
-	this.id = id;
-}
+	@Column(name = "subtitulo",length = 60, nullable = false)
+	private String           subTitulo;
 
-public String getIsbn() {
-	return isbn;
-}
+	@Column(name = "autor",length = 40, nullable = false)
+	private String           autor;
+	
+	@Column(name = "status", nullable = false)
+	private Boolean          status;
 
-public void setIsbn(String isbn) {
-	this.isbn = isbn;
-}
+	public Integer getId() {
+		return id;
+	}
 
-public String getDescricao() {
-	return descricao;
-}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public void setDescricao(String descricao) {
-	this.descricao = descricao;
-}
+	public String getIsbn() {
+		return isbn;
+	}
 
-public String getTitulo() {
-	return titulo;
-}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-public void setTitulo(String titulo) {
-	this.titulo = titulo;
-}
+	public String getDescricao() {
+		return descricao;
+	}
 
-public String getSubTitulo() {
-	return subTitulo;
-}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-public void setSubTitulo(String subTitulo) {
-	this.subTitulo = subTitulo;
-}
-   
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getSubTitulo() {
+		return subTitulo;
+	}
+
+	public void setSubTitulo(String subTitulo) {
+		this.subTitulo = subTitulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
 
 }
