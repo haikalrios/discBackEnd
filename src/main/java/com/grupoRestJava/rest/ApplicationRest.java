9c0cd39812aa78929f801e4f221ce49a93151f8d
Your Name <you@example.com>
2017-09-12 17:50:20 -0300

    ajustes no descriptor

--- src/main/java/com/grupoRestJava/rest/ApplicationRest.java
+++ src/main/java/com/grupoRestJava/rest/ApplicationRest.java
@@ -1,6 +1,9 @@
 package com.grupoRestJava.rest;
 
-//@ApplicationPath("/rest")
-public class ApplicationRest {//extends Application{
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ApplicationRest extends Application{
 
 }
